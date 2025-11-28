941021715b6f834dcb6976f384f230664c8ecfc0
someguy5583 <ritvik2g@gmail.com>
2025-11-28 16:59:50 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <body className="bg-neutral-400">
+        <div className="container">
+          
+        </div>
+      </body>
+      
+    </> 
+  )
+}
+
+export default App
